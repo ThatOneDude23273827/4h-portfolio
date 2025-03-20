7fb16a32c1316693b2d7757f5f3c1b354c6471c2
ThatOneDude2664 <cipickering2010@gmail.com>
2025-03-20 11:16:55 -0500

    Add files via upload

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -419,39 +419,4 @@ export class Start extends Phaser.Scene {
             this.loadPdf(section, xIfLoaded);
         };
     };
-
-    getMimeType(file) {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onloadend = function(e) {
-            const arr = (new Uint8Array(e.target.result)).subarray(0, 4);
-            let header = '';
-            for (let i = 0; i < arr.length; i++) {
-                header += arr[i].toString(16);
-            }
-            let type = '';
-            switch (header) {
-                case '89504e47':
-                type = 'image/png';
-                break;
-                case '47494638':
-                type = 'image/gif';
-                break;
-                case 'ffd8ffe0':
-                case 'ffd8ffe1':
-                case 'ffd8ffe2':
-                type = 'image/jpeg';
-                break;
-                default:
-                type = 'unknown';
-                break;
-            }
-            resolve(type);
-            };
-            reader.onerror = function() {
-            reject(new Error('File reading error'));
-            };
-            reader.readAsArrayBuffer(file);
-        });
-    };
 };
